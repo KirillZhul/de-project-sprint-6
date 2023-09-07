644bfa0d44b27e91bd0c37311108a87d294501ea
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-09-07 17:46:59 +0300

    Create create_table_dwh_l_user_group_activity.sql

---
+++ src/sql/create_table_dwh_l_user_group_activity.sql
@@ -0,0 +1,13 @@
+--drop table if exists STV2023081256__DWH.l_user_group_activity cascade;
+
+create table STV2023081256__DWH.l_user_group_activity
+(
+hk_l_user_group_activity integer PRIMARY KEY,
+hk_user_id int not null,
+hk_group_id int not null,
+load_dt timestamp not null,
+load_src VARCHAR(20)
+);
+
+ALTER TABLE STV2023081256__DWH.l_user_group_activity ADD CONSTRAINT fk_uga_users_hk_user_id FOREIGN KEY (hk_user_id) references STV2023081256__DWH.h_users(hk_user_id);
+ALTER TABLE STV2023081256__DWH.l_user_group_activity ADD CONSTRAINT fk_uga_groups_hk_group_id FOREIGN KEY (hk_group_id) references STV2023081256__DWH.h_groups(hk_group_id);
