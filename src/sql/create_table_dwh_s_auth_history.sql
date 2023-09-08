1a82400c529fdf87e63a0e05231c56b744f8602c
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-09-08 10:11:32 +0300

    Update create_table_dwh_s_auth_history.sql

--- src/sql/create_table_dwh_s_auth_history.sql
+++ src/sql/create_table_dwh_s_auth_history.sql
@@ -1,13 +1,14 @@
 --drop table if exists STV2023081256__DWH.s_auth_history cascade;
 
-create table STV2023081256__DWH.s_auth_history
-(
-hk_l_user_group_activity integer,
-user_id_from int,
-event varchar(10) ,
-event_dt timestamp,
-load_dt timestamp not null,
-load_src VARCHAR(20)
+create table STV2023081256__DWH.s_auth_history (
+  hk_l_user_group_activity integer, 
+  user_id_from int, 
+  event varchar(10), 
+  event_dt timestamp, 
+  load_dt timestamp not null, 
+  load_src VARCHAR(20)
 );
-
-ALTER TABLE STV2023081256__DWH.s_auth_history ADD CONSTRAINT fk_auth_hist_uga FOREIGN KEY (hk_l_user_group_activity) references STV2023081256__DWH.l_user_group_activity(hk_l_user_group_activity);
+ALTER TABLE 
+  STV2023081256__DWH.s_auth_history 
+ADD 
+  CONSTRAINT fk_auth_hist_uga FOREIGN KEY (hk_l_user_group_activity) references STV2023081256__DWH.l_user_group_activity(hk_l_user_group_activity);
