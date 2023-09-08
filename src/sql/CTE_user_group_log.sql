b3f352bb12f11ad55ce8502e71c915581fea4480
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-09-08 10:13:58 +0300

    Update CTE_user_group_log.sql

--- src/sql/CTE_user_group_log.sql
+++ src/sql/CTE_user_group_log.sql
@@ -1,18 +1,28 @@
 with user_group_log as (
-select hg.hk_group_id, 
-  hg.registration_dt, 
-  count(DISTINCT luga.hk_user_id) as cnt_added_users
-from STV2023081256__DWH.h_groups as hg 
-left join STV2023081256__DWH.l_user_group_activity as uga on uga.hk_group_id = hg.hk_group_id
-left join STV2023081256__DWH.s_auth_history as sah on sah.hk_l_user_group_activity = uga.hk_l_user_group_activity 
-where sah.event = 'add'
-group by hg.hk_group_id, hg.registration_dt
-order by hg.registration_dt 
-limit 10
-)
-select hk_group_id,
-       cnt_added_users
-from user_group_log
-order by cnt_added_users
-limit 10
-;
+  select 
+    hg.hk_group_id, 
+    hg.registration_dt, 
+    count(DISTINCT luga.hk_user_id) as cnt_added_users 
+  from 
+    STV2023081256__DWH.h_groups as hg 
+    left join STV2023081256__DWH.l_user_group_activity as uga on uga.hk_group_id = hg.hk_group_id 
+    left join STV2023081256__DWH.s_auth_history as sah on sah.hk_l_user_group_activity = uga.hk_l_user_group_activity 
+  where 
+    sah.event = 'add' 
+  group by 
+    hg.hk_group_id, 
+    hg.registration_dt 
+  order by 
+    hg.registration_dt 
+  limit 
+    10
+) 
+select 
+  hk_group_id, 
+  cnt_added_users 
+from 
+  user_group_log 
+order by 
+  cnt_added_users 
+limit 
+  10;
